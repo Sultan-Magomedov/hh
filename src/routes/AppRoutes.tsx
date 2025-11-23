d124f09c2827a4b5337cedf4b9667b2ce81642af
Magomedov Sultan <sultanatus008@mail.ru>
2025-11-23 03:34:14 +0300

    edits

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
 import { Vacancies } from "../pages/Vacancies";
 import { Vacancy } from "../pages/Vacancy";
 import { MainLayout } from "../layouts/MainLayout";
@@ -9,6 +9,7 @@ export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
+        <Route index element={<Navigate to="vacancies" replace />} />
         <Route path="vacancies" element={<Vacancies />}>
           <Route path="moscow" element={<TabsCity />} />
           <Route path="petersburg" element={<TabsCity />} />
