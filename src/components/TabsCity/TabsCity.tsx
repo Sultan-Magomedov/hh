39a5d179fdcb59250869f5ea627d6e0c2f50fe89
Magomedov Sultan <sultanatus008@mail.ru>
2025-11-23 03:31:49 +0300

    edits

--- src/components/TabsCity/TabsCity.tsx
+++ src/components/TabsCity/TabsCity.tsx
@@ -1,6 +1,6 @@
 import { Tabs } from "@mantine/core";
 import styles from "./TabsCity.module.css";
-import { useNavigate, useSearchParams } from "react-router";
+import { useNavigate } from "react-router";
 import { useTypedDispatch, useTypedSelector } from "../../hooks/redux";
 import { useEffect } from "react";
 import { fetchVacancies } from "../../store/reducers/searchSlice";
@@ -8,11 +8,8 @@ import { setCityId } from "../../store/reducers/filterSlice";
 
 export const TabsCity = () => {
   const navigate = useNavigate();
-  const { idCity, skills, name } = useTypedSelector(
-    (state) => state.filterReducer
-  );
+  const { idCity } = useTypedSelector((state) => state.filterReducer);
   const dispatch = useTypedDispatch();
-  const [searchParams, setSearchParams] = useSearchParams();
 
   const changeHandler = (value: string) => {
     if (!value) return;
@@ -23,21 +20,6 @@ export const TabsCity = () => {
     if (path) {
       navigate(`${path}`);
     }
-
-    if (skills.length > 0) {
-      searchParams.set("skills", skills.join(","));
-    } else {
-      searchParams.delete("skills");
-    }
-
-    if (name) {
-      searchParams.set("text", name);
-    } else {
-      searchParams.delete("text");
-    }
-
-    searchParams.set("area", idCity);
-    setSearchParams(searchParams);
 
     dispatch(fetchVacancies());
   };
