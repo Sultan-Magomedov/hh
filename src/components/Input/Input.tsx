80a7673fe008f8d45033d5546cf77f32bf14628f
Magomedov Sultan <sultanatus008@mail.ru>
2026-06-19 11:05:55 +0300

    fix:input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,27 +1,28 @@
-import { Loader, TextInput } from "@mantine/core"
+import { TextInput } from "@mantine/core"
 import "@mantine/core/styles.css"
 import Search from "../../assets/icons/search.svg?react"
 
 import { useTypedDispatch, useTypedSelector } from "../../hooks/redux"
 import { setName } from "../../store/reducers/filterSlice"
-import React, { useEffect, useState, useTransition } from "react"
+import React, { useEffect, useState } from "react"
+import { useDebouncedValue } from "../../hooks/useDebouncedValue"
 
 export const Input = () => {
 	const { name } = useTypedSelector((state) => state.filterReducer)
 	const dispatch = useTypedDispatch()
 	const [value, setValue] = useState(name)
-	const [isPending, startTransition] = useTransition()
+	const debouncedValue = useDebouncedValue(value, 500)
 
 	useEffect(() => {
 		setValue(name)
 	}, [name])
 
+	useEffect(() => {
+		dispatch(setName(debouncedValue))
+	}, [debouncedValue, dispatch])
+
 	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const newValue = e.currentTarget.value
-		setValue(newValue)
-		startTransition(() => {
-			dispatch(setName(newValue))
-		})
+		setValue(e.currentTarget.value)
 	}
 
 	return (
@@ -34,7 +35,6 @@ export const Input = () => {
 			size="md"
 			radius="md"
 			placeholder="Должность или название компании"
-			rightSection={isPending ? <Loader size="xs" /> : null}
 		/>
 	)
 }
